f5ec291c63c53d1a788fbd715c22ddcafa8a60be
Mike Hoitomt <mike.hoitomt@gmail.com>
2019-04-03 18:22:46 -0500

    Add team events

---
+++ db/migrate/20190403230026_create_team_manager_events.team_manager.rb
@@ -0,0 +1,15 @@
+# This migration comes from team_manager (originally 20190403224632)
+class CreateTeamManagerEvents < ActiveRecord::Migration[5.2]
+  def change
+    create_table :team_manager_events do |t|
+      t.integer :team_id
+      t.date :start_date
+      t.date :stop_date
+      t.string :description
+      t.string :location
+      t.string :venue
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_30_165545) do
+ActiveRecord::Schema.define(version: 2019_04_03_230026) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,6 +34,17 @@ ActiveRecord::Schema.define(version: 2019_03_30_165545) do
     t.string "checksum", null: false
     t.datetime "created_at", null: false
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
+
+  create_table "team_manager_events", force: :cascade do |t|
+    t.integer "team_id"
+    t.date "start_date"
+    t.date "stop_date"
+    t.string "description"
+    t.string "location"
+    t.string "venue"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "team_manager_player_photos", force: :cascade do |t|
