817b5ac756179a886a7009c0cda395ed6aebc8f3
Mike Hoitomt <mike.hoitomt@gmail.com>
2019-04-24 22:43:08 -0500

    Add the Trix editor

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -11,3 +11,5 @@ Rails.application.config.assets.version = '1.0'
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
 Rails.application.config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
+Rails.application.config.assets.precompile += %w( trix.css )
+Rails.application.config.assets.precompile += %w( team_manager/trix.css )
